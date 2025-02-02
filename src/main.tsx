3b92c6ccb780263dbff1d615d4d7f4ad8b8e1fc5
equanime <Adragna.yaya@gmail.com>
2025-02-02 09:29:36 -0500

    correction main rout for APP

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../../portfolio/src/App';
+import App from '../src/App';
 
 console.log('main.tsx is running');
 console.log(document.getElementById('root'));
