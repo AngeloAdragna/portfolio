b5fd5765aa645d85f22ff4eb50793f05013a2fef
equanime <Adragna.yaya@gmail.com>
2025-02-02 09:49:11 -0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,5 @@ console.log('main.tsx is running');
 console.log(document.getElementById('root'));
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
