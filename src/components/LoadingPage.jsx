94439cdf4f592ab66ad622a894905402a5c1c009
AngeloAdragna <angelo.adragnadev@gmail.com>
2025-03-01 20:22:25 +0100

    laptop size

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -4,7 +4,7 @@ import '../styles/LoadingPage.css';
 const LoadingPage = () => {
     return (
         <section id="loading">
-            <div className="loading-">
+            <div className="loading-container">
                 <div className="spinner"></div>
                 <p>Chargement...</p>
             </div>
